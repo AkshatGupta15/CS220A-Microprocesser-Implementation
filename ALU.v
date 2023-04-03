cfd61a6a2f1654ca045d8dbf5bed43b3bd3861f9
Akshat <akshatg20@iitk.ac.in>
2023-04-03 21:39:28 +0530

    ALU and branching

---
+++ ALU.v
@@ -0,0 +1,98 @@
+`timescale 1ns / 1ps
+
+module ADD (input signed [31:0] A, input signed [31:0] B, output reg [31:0] out);
+
+	 
+always@(*) begin
+  out = A + B;
+end  
+
+endmodule
+
+module ADDI(input signed [31:0] A, input [15:0] imm,  output reg [31:0] out);
+
+always @(*) begin
+  out = A + {{16{imm[15]}}, imm}; // Sign-extend the immediate value
+end
+
+endmodule
+
+module ADDIU(input unsigned [31:0] A, input [15:0] imm,  output reg [31:0] out);
+
+always @(*) begin
+  out = A + {{16{imm[15]}}, imm}; // Sign-extend the immediate value
+end
+
+endmodule
+
+module ADDU(input unsigned [31:0] A, input unsigned [31:0] B, output reg [31:0] out);
+	 
+always@(*) begin
+  out = A + B;
+end  
+
+endmodule
+
+module SUB(input signed [31:0] A, input signed [31:0] B, output reg [31:0] diff);
+	 
+always@(*) begin
+  diff = A - B;
+end  
+
+endmodule
+
+module SUBU(input unsigned [31:0] A, input unsigned [31:0] B, output reg [31:0] diff);
+	 
+always@(*) begin
+  diff = A - B;
+end  
+
+endmodule
+
+module AND(input [31:0] A, input [31:0] B, output reg [31:0] out);
+	 
+    always@(*) begin
+    out = A & B;
+    end  
+
+endmodule
+
+module ANDI(input [31:0] A, input [15:0] imm, output reg [31:0] out);
+
+    always @(*) begin
+    out = A & {{16{imm[15]}}, imm}; // Sign-extend the immediate value
+    end
+
+endmodule
+
+module OR(input [31:0] A, input [31:0] B, output reg [31:0] out);
+	 
+    always@(*) begin
+    out = A | B;
+    end  
+
+endmodule
+
+module ORI(input [31:0] A, input [15:0] imm, output reg [31:0] out);
+
+    always @(*) begin
+    out = A | {{16{imm[15]}}, imm}; // Sign-extend the immediate value
+    end
+
+endmodule
+
+module SLL(input [31:0] A, input [15:0] sa, output reg [31:0] out);
+
+  always @(*) begin
+    out = A << {{16{sa[15]}}, sa};
+  end
+
+endmodule
+
+module SRL(input [31:0] A, input [15:0] sa, output reg [31:0] out);
+
+  always @(*) begin
+    out = A >> {{16{sa[15]}}, sa};
+  end
+
+endmodule
--- branching.v
+++ branching.v
@@ -6,6 +6,7 @@ module beq (clk, rst, r0, r1, offset, pc_in, pc_out);
   input [31:0] r0, r1;
   input [15:0] offset;
   input [31:0] pc_in;
+
   output reg [31:0] pc_out;
 
   always @(posedge clk or posedge rst) begin
@@ -13,7 +14,7 @@ module beq (clk, rst, r0, r1, offset, pc_in, pc_out);
       pc_out <= 0;
     end
     else if (r0 == r1) begin
-      pc_out <= pc_in + 4 + offset;
+      pc_out <= pc_in + offset + 4;
     end
     else begin
       pc_out <= pc_in + 4;
@@ -129,5 +130,55 @@ module bne (clk, rst, r0, r1, offset, pc_in, pc_out);
       pc_out <= pc_in + 4;
     end
   end
+
+endmodule
+
+module j (
+    input [25:0] target_address,
+    input [31:0] pc_in,
+    output reg [31:0] pc_out,
+    output reg jump_taken
+);
+
+    always @(*) begin
+        pc_out = {pc_in[31:28], target_address, 2'b00}; // set jump target address
+        jump_taken = 1'b1; // set jump taken signal
+    end
+
+endmodule
+
+// The 'j' instruction is an unconditional jump that transfers
+// control to the instruction at the target address. The jump address 
+// is calculated by concatenating the upper 4 bits of the current program counter 
+// with the 26-bit jump address left-shifted by 2 bits. 
+
+
+module jal(
+    input [25:0] target_address,
+    input [31:0] pc_in,
+    output reg [31:0] pc_out,
+    output reg jump_taken,
+    output reg [31:0] jal_ra
+);
+
+    always @(*) begin
+        pc_out = {pc_in[31:28], target_address, 2'b00}; // set jump target address
+        jump_taken = 1'b1; // set jump taken signal
+        jal_ra = pc_in + 4;
+    end
+
+    
+endmodule
+
+module jr (
+    input [31:0] r0,
+    output reg [31:0] pc_out,
+    output reg jump_taken
+);
+
+    always @(*) begin
+        pc_out = r0; // set jump target address to value in r0 register
+        jump_taken = 1'b1; // set jump taken signal
+    end
 
 endmodule
